76b31a1c23121b25418e5875001dc0c122e7af0c
geoffhough <geoff.hough@gmail.com>
2024-11-14 18:20:53 -0800

    Chapter 2 Assignment Module 9
    intro to javascript

---
+++ Module 09/main.js
@@ -0,0 +1,56 @@
+// CHAPTER 19 AND CHAPTER 2 ASSIGNMENT - INTRO TO JAVASCRIPT
+// ===================================
+
+// Please download the assignment file here:
+
+// https://drive.google.com/file/d/14L3pL6JeCuDI-FzAIe7TzRpMSzsNLzD-/view?usp=drivesdk
+
+// Here is a link to a video on how to do the assignment:
+
+// https://youtu.be/FDyz8NvnBxM
+
+// Please upload your *saved* file or Copy and paste your code into an email message. Either works fine.
+
+// Details:
+
+// Read the instructions that are found within the file that you have downloaded (labeled as "main.js").
+//  Be sure to declare and name your variables properly. The lecture video, which the link will also
+//  be provided below, has the "answers" on how to do each question.
+
+// https://youtu.be/k7VnQs6YwQI
+
+// ==================================
+
+// declare a variable using the keyword "let" and the identifier aka the name of "cats"
+// and assign the value of 22
+
+let cats = 22;
+
+// declare a variable using the identifier aka the name of "dogs" and assign the value of 13
+
+let dogs = 13;
+
+// declare a variable using the identifier aka the name of "addingVariables" and
+//  assign the expression of "cats + dogs"
+
+let addingVariables = cats + dogs;
+
+// declare a variable using the identifier aka the name of "addingStrings" and
+// concat 'javascript is ' + 'really awesome!'
+
+let addingStrings = "javascript is " + "really awesome!";
+
+// declare an array using the identifier aka the name of "myArray" and add these
+//  three items into it: 'Coding is awesome', 500, false
+
+let myArray = ["Coding is awesome", 500, false];
+
+// resassign myArray[1] to 250
+
+myArray[1] = 250;
+
+// create your own variable and assign a random string/message to it
+
+let MyOwnVariable = "We use JavaScript, not Python.";
+
+main.js;
